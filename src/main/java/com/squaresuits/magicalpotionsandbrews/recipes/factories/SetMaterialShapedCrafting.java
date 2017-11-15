3db20ffba7a495ce9319c152dcb932f2e8e53641
Jeff Baker <racerdelux@gmail.com>
2017-11-14 20:31:54 -0600

    first factory

--- src/main/java/com/squaresuits/magicalpotionsandbrews/recipes/factories/SetMaterialShapedCrafting.java
+++ src/main/java/com/squaresuits/magicalpotionsandbrews/recipes/factories/SetMaterialShapedCrafting.java
@@ -1,4 +1,15 @@
 package com.squaresuits.magicalpotionsandbrews.recipes.factories;
 
-public class SetMaterialShapedCrafting {
+import com.google.gson.JsonObject;
+import com.squaresuits.magicalpotionsandbrews.recipes.PABShapedOreRecipe;
+import net.minecraft.util.JsonUtils;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.common.crafting.IRecipeFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+public class SetMaterialShapedCrafting implements IRecipeFactory{
+    @Override
+    public IRecipe parse(JsonContext context, JsonObject json) {
+        return new PABShapedOreRecipe(JsonUtils.getString(json,"material"));
+    }
 }
